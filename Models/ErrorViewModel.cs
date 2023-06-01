ef11140dd7475cbd2d765b8d7270d6d28879e01d
hasira09 <hasiramahel09@gmail.com>
2023-06-01 17:05:39 +0530

    CRED Application

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ASP_dotnet_Core_WebApps.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
